bf30f7b277ddabea256f2cc45c26df388446c7b4
AhmedGamal <58227700+AhmedGamal77823@users.noreply.github.com>
2020-02-25 13:57:18 +0200

    Update LinkedList.cpp

--- DS/LinkedList.cpp
+++ DS/LinkedList.cpp
@@ -1,74 +1,114 @@
-//
-// Created by AhmedGamal on 2/16/20.
-//
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5+10;
+#define ll long long
+const int N = 1e3+10, OO = 0x3f3f3f3f;
 
 struct Node{
     int data;
-    Node *link;
+    Node *Link;
 };
 
 class LinkedList{
-private:
     Node *head, *tail;
 public:
     LinkedList(){
-        head = NULL;
-        tail = NULL;
-    }
-    void makeNode(int val){
-        Node *temp = new Node;
-        temp->data = val;
-        temp->link = NULL;
-
-        if(head == NULL){
-            head = temp;
-            tail = temp;
-            temp = NULL;
-        }else{
-            tail ->link = temp;
-            tail = temp;
-        }
+        head = tail = NULL;
     }
 
-    void Display(){
-        Node *temp = new Node;
-        temp = head;
-        while(temp != NULL){
-            cout << temp->data << " ";
-            temp = temp->link;
-        }
-    }
+    void CreateNode(int value){
 
-    void inserInStart(int value){
         Node *temp = new Node;
         temp->data = value;
-        temp->link = head;
+        temp->Link = NULL;
+
+        if(head == NULL){
+            tail = head = temp;
+        }
+        else{
+            tail->Link = temp;
+            tail = temp;
+        }
+    }
+
+    void InsertStart(int value){
+        Node *temp = new Node;
+        temp->data = value;
+        temp->Link = head;
         head = temp;
     }
 
-    void InsertPosition(int pos, int val){
-        Node *cur = new Node;
+    void InsertPos(int pos, int value){
         Node *pre = new Node;
+        Node *cur = new Node;
         Node *temp = new Node;
 
         cur = head;
         for(int i = 1 ; i < pos ; ++i){
             pre = cur;
-            cur = cur->link;
+            cur = cur->Link;
         }
+        temp->data = value;
 
-        temp->data = val;
-        pre->link = temp;
-        temp->link = cur;
+        temp->Link = cur;
+        pre->Link = temp;
+    }
+
+    void DeleteFirst(){
+        Node *temp = new Node;
+        temp = head;
+        head = head->Link;
+        delete(temp);
+    }
+
+    void DeleteEnd(){
+        Node *pre = new Node;
+        Node *cur = new Node;
+
+        cur = head;
+        while(cur->Link != NULL){
+            pre = cur;
+            cur = cur->Link;
+        }
+        tail = pre;
+        tail->Link = NULL;   //pre -> Link = NULL
+        delete(cur);
+    }
+
+    void DeletePos(int pos){
+        Node *cur = new Node;
+        Node *pre = new Node;
+
+        cur = head;
+        for(int i = 1 ; i < pos ; ++i){
+            pre = cur;
+            cur = cur->Link;
+        }
+        pre->Link = cur->Link;
+        delete(cur);
+    }
+    void Display(){
+        Node *temp = new Node;
+        temp = head;
+        while(temp != NULL){
+            cout << temp->data << "   " << temp->Link << endl;
+            temp = temp->Link;
+        }
     }
 };
 
 int main() {
+    LinkedList l;
 
+    l.CreateNode(20);
+    l.CreateNode(30);
+    l.CreateNode(40);
+    l.InsertStart(10);
+    l.InsertPos(2, 100);
+    l.DeletePos(2);
+    l.DeleteFirst();
+    l.DeleteEnd();
+
+    l.Display();
     return 0;
 }
-
