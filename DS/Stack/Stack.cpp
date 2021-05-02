eb7d5c9e593e87dbfc1a1f0010a52867c2c29d34
AhmedGamal77823 <ahmedgamale.1019@gmail.com>
2021-05-02 12:30:13 +0200

    bulding the functions

--- DS/Stack/Stack.cpp
+++ DS/Stack/Stack.cpp
@@ -1,17 +1,33 @@
 #include <bits/stdc++.h>
 #include <Stack.h>
 
-struct Node{
-    int data;
-    Node *link;
-};
 
-class stack{
+template<typename T>
+void Stack<T>::push(int value){ 
+    Node *holder = new Node;
+    holder -> data = value;
+    holder -> link = NULL;
     
-    private:
-    Node *head, *tail;
+    if(NodesCounter == 0){head = tail = holder;}
+    else{
+        tail -> link = holder;
+    }
+    ++NodesCounter;
+    delete (holder);
+}
 
-};
-int main(){
+template<typename T>
+void Stack<T>::pop(){
+    Node *cur = new Node;
+    Node *pre = new Node;
 
+    cur = tail;
+    pre = head;
+    for(int i = 1; i <= NodesCounter-1; ++i){
+        pre = pre->link;
+    }
+
+    tail = pre;
+    tail->link = NULL;
+    delete(cur);
 }
--- DS/Stack/Stack.h
+++ DS/Stack/Stack.h
@@ -13,13 +13,13 @@ private:
     Node *head, *tail;
     int NodesCounter;
     bool isEmpty;
+
+public:
     Stack(){
         head = tail = NULL;
         NodesCounter = 0;
         isEmpty = 0;
     }
-
-public:
     void push(int value);
     void pop();
     int top();
